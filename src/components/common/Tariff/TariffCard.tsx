e207004130ebc15d0f93ec0627ddcc527ec36f29
tab11pm <tabrezshonizorov@gmail.com>
2026-01-26 21:55:33 +0700

    fix: ui error

--- src/components/common/Tariff/TariffCard.tsx
+++ src/components/common/Tariff/TariffCard.tsx
@@ -18,18 +18,18 @@ export const TariffCard = ({ card, onClick, isActive }: TariffCardProps) => {
 	return (
 		<Card
 			key={card.id}
-			className={` card-background relative overflow-hidden border-2 ${isActive ? 'border-(--primary)' : 'border-[#484D4E]'} px-8 py-10 hover:-translate-y-2 hover:translate-x-1 cursor-pointer  transition-transform ease-in duration-150 hover:shadow-lg  relative shadow-orange-300`}
+			className={` card-background relative overflow-hidden border-2 ${isActive ? 'border-(--primary)' : 'border-[#484D4E]'} md:px-8 hover:-translate-y-2 hover:translate-x-1 cursor-pointer  transition-transform ease-in duration-150 hover:shadow-lg  relative shadow-orange-300`}
 		>
 			<button
-				className="block  md:overflow-hidden w-full h-full md:space-y-6"
+				className="block  w-full  md:space-y-6"
 				onClick={() => onClick(card.id)}
 			>
-				<div className="flex h-1 md:h-auto justify-end md:justify-between gap-2">
+				<div className="flex items-start justify-end md:justify-between gap-2 mb-2 h-10">
 					<div className="relative -top-5 md:absolute md:-top-1 rounded-lg  bg-(--red) h-10 w-16 flex items-center justify-center text-xl">
 						-{card.discountPercent}%
 					</div>
 					<div></div>
-					<div className="relative md:static -top-2 rounded-lg md:text-3xl text-(--primary) ">
+					<div className="relative -top-3 md:-top-4 rounded-lg md:text-3xl text-(--primary) ">
 						{card.is_best && 'хит!'}
 					</div>
 				</div>
